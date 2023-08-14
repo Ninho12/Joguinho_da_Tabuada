cb5f2c33e8a892d5a9c016a6943fe18989e733e5
Joao Paulo <joaopaulojpsp@gmail.com>
2023-08-14 09:43:07 -0300

    Commit importante

--- script.js
+++ script.js
@@ -10,3 +10,40 @@
 // Iniciando constantes importantes no jogo.
 
 const ESCOLHA = document.getElementById("escolha")
+const JOGO = document.getElementById("jogo")
+const MENSAGEM = document.getElementById("mensagem")
+
+
+// Criando função do jogo.
+function escolher(n, op){
+
+    var conteudo = ""
+
+    for(var i = 1; i <= 10; i++){
+
+        if(op == "+"){
+            conteudo += "<tr>"
+            conteudo += "<td>"+ n+" + "+i+" = "+(n+1)+"</td>"
+            conteudo += "</tr>"  
+        }
+        else if(op == "-"){
+            conteudo += "<tr>"
+            conteudo += "<td>"+ n+" - "+i+" = "+(n-1) +"</td>"
+            conteudo += "</tr>"
+        }
+        else if(op == "x"){
+            conteudo += "<tr>"
+            conteudo += "<td>" + n+" x "+i+" = "+(n*1) + "</td>"
+            conteudo += "</tr>"  
+        }
+        else if(op == "/"){
+            conteudo += "<tr>"
+            conteudo += "<td>" + n+" / "+i+" = "+(n/1) +"</td>"
+            conteudo += "</tr>"
+        }
+
+
+        
+    }
+
+}
