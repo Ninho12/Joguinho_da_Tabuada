d1255cafa62b515093f1fb4b2b6454dbfb9a15b4
Joao Paulo <joaopaulojpsp@gmail.com>
2023-08-14 17:09:06 -0300

    Estou quase terminando o jogo

--- script.js
+++ script.js
@@ -20,6 +20,15 @@ var array_da_tabuada = [];
 // O contador da questão
 var contador = 0
 
+// Pontuacao
+var score = 0
+
+
+// Pegando os elementos
+var pontuacao = document.getElementById("pontuacao")
+var questoes = document.getElementById("questoes")
+var campo = document.getElementById("resposta")
+
 
 // Criando função do jogo.
 function escolher(n, op){
@@ -89,19 +98,97 @@ function escolher(n, op){
 // Função para começar o jogo
 function comecar(){
 
+    // Configurações iniciais
     CONTAINER.removeChild(ESCOLHA)
     JOGO.style.visibility = "visible"
+
+    // pegando os valores
+    var n1 = array_da_tabuada[contador].primeiroNumero
+    var n2 = array_da_tabuada[contador].segundoNumero
+    var op = array_da_tabuada[contador].operacao
+
+    // Mudando os valores
+    pontuacao.innerHTML = "<b>Pontuacao: 0</b>"
+    questoes.innerHTML = n1+" "+ op +" "+n2+" = ?"
 
 }
 
 // Função para o a realização do jogo
 function proximo(){
 
+   var campo = document.getElementById("resposta")
    
+   console.log(campo)
+
+   var numero = parseInt(campo.value)
+
+   if(acertou(numero)){
+        atualizar()
+   }else{
+        game_over()
+   }
 
 }
 
+// Verificar se acertou ou errou
+function acertou(n){
 
+    var resultado = array_da_tabuada[contador].resultado
+    if(n === resultado){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+// Função para atualizar as questoes
+function atualizar(){
+
+
+    if(contador < 10){
+        // Quando o jogo estiver rodando
+        var res = array_da_tabuada[contador].resultado
+        score += res
+        contador = contador + 1
+
+        // pegando os valores
+        var n1 = array_da_tabuada[contador].primeiroNumero
+        var n2 = array_da_tabuada[contador].segundoNumero
+        var op = array_da_tabuada[contador].operacao
+        
+
+        // Atualizando de fato
+        pontuacao.innerHTML = "<b>Pontuacao: "+score+"</b>"
+        questoes.innerHTML = n1+" "+ op +" "+n2+" = ?"
+        campo.value = ""
+        
+    }else{
+        // Quando o jogo chegou ao fim
+
+
+    }
+
+}
+// Função para game over
+function game_over(){
+    var texto = ""
+
+    CONTAINER.removeChild(JOGO)
+    MENSAGEM.style.visibility = 'visible'
+
+    // Mensagem
+    texto += "<h2>Você Perdeu!</h2>"
+    texto += "<p>Não desanime não! Você pode tentar novamente!</p>"
+
+
+    // Botões
+    texto += "<a class='botao' href='#' onclick='location.reload()'>Tentar Novamente</a>"
+    texto += "<a class='botao' href='index.html'>Ir Para o Menu</a>"
+
+    // Inserindo o conteudo
+    MENSAGEM.innerHTML = texto
+}
 
 
 // Função de Construção do objeto tabuada
