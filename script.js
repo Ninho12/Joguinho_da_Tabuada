3643b12cd4a5eb1f485bfecf4b1a4343aba3b578
Joao Paulo <joaopaulojpsp@gmail.com>
2023-08-14 04:07:57 -0300

    Terceiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+    Arquivo responsavel por toda a logica do
+    joguinho da tabuada.
+
+    Vou criar 4 funções relacionada as operações que vai
+    executar todos os calculos.
+*/
+
+
+// Iniciando constantes importantes no jogo.
+
+const ESCOLHA = document.getElementById("escolha")
